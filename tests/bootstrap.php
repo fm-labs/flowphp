f77a78f2f95c70f39d66dc24fbed7ef28ae4702d
fm-labs <flowmotionlabs@gmail.com>
2019-11-10 22:26:21 +0100

    Initial

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,12 +4,14 @@ date_default_timezone_set('UTC');
 $root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
 
 $loader = require $root . 'vendor/autoload.php';
+/*
 $loader->add('Flow\\', $root . 'src/');
 $loader->add('Flow\\Test\\', $root . 'tests/');
 
 $loader->add('Flow\\', $root . 'dev/');
 $loader->add('Flow\\Test\\', $root . 'dev/tests/');
+*/
 
-$loader->add('FlowTestApp\\', $root . 'tests/test_app/src/');
+$loader->add('Flow\\TestApp\\', $root . 'tests/test_app/src/');
 
 require_once $root . 'src/functions.php';
